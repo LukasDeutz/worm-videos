26309a79a7a7c5760d31140b61bb352ffdebfb1e
Lukas <scld@leeds.ac.uk>
2024-03-06 11:49:53 +0000

    adds paths to minima-worm repo

--- worm_videos/data_dirs.py
+++ worm_videos/data_dirs.py
@@ -6,8 +6,25 @@ Created on 6 Mar 2024
 
 from pathlib import Path
 
-result_path = Path('../../results/')
-data_path = result_path / 'data/undulation'
-log_dir = data_path / 'logs'
-sim_dir = data_path / 'simulations'
-sweep_dir = data_path / 'parameter_sweeps'
+
+# Paths to data directory in the minimal-worm repository
+
+data_path = Path('../../minimal-worm/minimal_worm/experiments/results/data')
+
+data_path_undu = data_path / 'undulation'
+data_path_actu_rexa = data_path / 'actuation_relaxation'
+
+log_dir_undu = data_path_undu / 'logs'
+sim_dir_undu = data_path_undu / 'simulations'
+sweep_dir_undu = data_path_undu / 'parameter_sweeps'
+
+log_dir_actu_rexa = data_path_undu / 'logs'
+sim_dir_actu_rexa = data_path_undu / 'simulations'
+sweep_dir_actu_rexa = data_path_undu / 'parameter_sweeps'
+
+for Dir in [log_dir_undu, sim_dir_undu, sweep_dir_undu, 
+    log_dir_actu_rexa, sim_dir_actu_rexa, sweep_dir_actu_rexa]:
+
+    assert Dir.is_dir()
+
+
