6756ebf52d7a7c8a5efef1367f47cfd192e75e32
Lukas <scld@leeds.ac.uk>
2024-03-06 11:32:25 +0000

    Adds files

---
+++ worm_videos/data_dirs.py
@@ -0,0 +1,13 @@
+'''
+Created on 6 Mar 2024
+
+@author: amoghasiddhi
+'''
+
+from pathlib import Path
+
+result_path = Path('../../results/')
+data_path = result_path / 'data/undulation'
+log_dir = data_path / 'logs'
+sim_dir = data_path / 'simulations'
+sweep_dir = data_path / 'parameter_sweeps'
