ef0171aa7363d3c18f48a43606c587d0a036534c
Lukas <scld@leeds.ac.uk>
2024-03-06 19:01:20 +0000

    adds dir for storage

--- worm_videos/data_dirs.py
+++ worm_videos/data_dirs.py
@@ -28,5 +28,29 @@ for Dir in [log_dir_undu, sim_dir_undu, sweep_dir_undu,
     assert Dir.is_dir()
 
 
+storage_dir = Path('/home/lukas/storage/minimal-worm/results/')  
+base_path = Path('/home/lukas/git/minimal-worm/minimal_worm/experiments/')
+
+storage_dir = storage_dir.relative_to('/home/lukas/')
+base_path = base_path.relative_to('/home/lukas/')
+relative_path = Path(*(['../']*len(base_path.parts)))
+
+storage_dir = relative_path / storage_dir 
+storage_dir.mkdir(parents = True, exist_ok = True)
+
+def get_storage_dir(experiment_type):
+
+    storage_dir_exp = storage_dir / experiment_type
+    
+    assert storage_dir_exp.isdir()
+                                                                                 
+    log_dir = storage_dir / 'logs'
+    sim_dir = storage_dir / 'simulations'
+    sweep_dir= storage_dir / 'parameter_sweeps'
+    
+    return log_dir, sim_dir, sweep_dir
+
+
+
 
 
