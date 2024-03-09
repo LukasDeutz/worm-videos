745c3ab6c727f9d934cb0d98acb0eb9a37786ecb
Lukas <scld@leeds.ac.uk>
2024-03-09 13:35:02 +0000

    debugs

--- worm_videos/viva.py
+++ worm_videos/viva.py
@@ -141,6 +141,77 @@ def make_video_undulation_sweep():
                 draw_e3 = False, 
                 n_arrows = 0.025)
 
+def generate_video(
+        filepath: Path,
+        r: np.ndarray,
+        d1: np.ndarray,
+        d2: np.ndarray,
+        d3: np.ndarray,
+        k: np.ndarray,
+        t: np.ndarray):
+    
+        FS = SimpleNamespace(**{
+            'r': r, 
+            'd1': d1,
+            'd2': d2,
+            'd3': d3,
+            'k': k,
+            't': t
+            }
+        )
+
+        studio = WormStudio(FS)
+
+        studio.generate_clip(
+            filepath, 
+            add_trajectory = False, 
+            draw_e3 = False, 
+            n_arrows = 0.025)
+
+        return
+    
+def showcase_dynamical_regimes():
+    
+    # Change file name
+    h5_filename = Path('raw_data_a=0.034_b=0.01_c_min=0.5_c_max=2.0_c_step=0.25_lam_min=0.5_lam_max=2.0_lam_step=0.25_N=750_dt=0.01_T=10.0.h5')    
+
+    h5, PG = load_data(h5_filename) 
+
+    a_arr, b_arr = PG.v_from_key('a'), PG.v_from_key('b')
+    
+    t = h5['t'][:]
+    #T = PG.base_parameter['T']    
+    fps = int(1.0 / PG.base_parameter['dt'])
+
+    # Regimes
+    a1, b1 = 0.4, 0.005
+    a2, b2 = 45, 0.1
+    a3, b3 = 2200, 0.4
+                        
+    video_dir = video_dir / 'undulation' / h5_filename.stem
+    video_dir.mkdir(parent=True, exist_ok = True)
+    
+    print(video_dir.resolve())
+
+    for n, (ai, bi) in enumerate(zip([a1, a2, a3], [b1, b2, b3])):
+        
+        i = np.abs(a_arr - ai).min()
+        j = np.abs(b_arr - bi).min()
+                                
+        l = np.ravel_multi_index((i,j), (len(a_arr), len(b_arr)))
+    
+        r = h5['FS']['r'][l, :]
+        d1 = h5['FS']['d1'][l, :]
+        d2 = h5['FS']['d2'][l, :]
+        d3 = h5['FS']['d3'][l, :]
+        k = h5['FS']['k'][l, :]
+        
+        filepath = video_dir / f'regime_{n+1}_a={ai}_b={bi}'
+        
+        generate_video(filepath, r, d1, d2, d3, k, t)
+                
+    return
+
 if __name__ == '__main__':
     
     make_video_actuation_relaxation_sweep()
